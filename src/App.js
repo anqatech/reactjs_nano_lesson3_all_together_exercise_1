3e2188c89d87dffeff216176423009860c640ef7
root <jalal@anqatech.com>
2021-12-31 18:46:27 +0000

    Finalized exercise

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import logo from './logo.svg'
+import './App.css'
+import NewUserInput from './NewUserInput'
+import UserList from './UserList'
 
 /*
 This exercise will help you put together and practice all of the concepts you've
@@ -11,6 +14,66 @@ The instructions for this project are located in the `instructions.md` file.
 */
 
 class App extends Component {
+  state = {
+    newUser: {
+      id: uuidv4(),
+      firstName: '',
+      lastName: '',
+      userName: '',
+      numberGames: 0
+    },
+    users: [
+      {
+        id: uuidv4(),
+        firstName: 'jalal',
+        lastName: 'el hazzat',
+        userName: 'the-king',
+        numberGames: 0
+      },
+      {
+        id: uuidv4(),
+        firstName: 'liam',
+        lastName: 'elhazzat',
+        userName: 'the-prince',
+        numberGames: 0
+      }
+    ],
+    isHidden: false
+  }
+
+  addItem = event => {
+    event.preventDefault();
+    this.setState(oldState => ({
+      users: [...oldState.users, this.state.newUser],
+      newUser: {
+        id: uuidv4(),
+        firstName: '',
+        lastName: '',
+        userName: '',
+        numberGames: 0
+      }
+    }))
+  }
+
+  handleInputChange = event => {
+    const name = event.target.name
+    const value = event.target.value
+
+    this.setState(() => ({ 
+      newUser: { ...this.state.newUser, [name]: value} 
+    }))
+  }
+
+  inputIsNotValid = () => {
+    return Object.values(this.state.newUser).some(x => (x === null || x === ''))
+  }
+
+  toggleIsHidden = () => {
+    this.setState(oldState => ({
+      isHidden: !oldState.isHidden
+    }))
+  }
+
   render() {
     return (
       <div className="App">
@@ -18,9 +81,24 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
+
+        <div className='grid-container'>
+          <NewUserInput
+            newUser={this.state.newUser}
+            addItem={this.addItem}
+            handleInputChange={this.handleInputChange}
+            inputIsNotValid={ this.inputIsNotValid }
+          />
+
+          <UserList 
+            users={ this.state.users }
+            toggleIsHidden={ this.toggleIsHidden }
+            isHidden={ this.state.isHidden }
+          />
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/UserList.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+
+const NewUserInput = ({ newUser, addItem, handleInputChange, inputIsNotValid }) => {
+
+    return (
+        <div className="create-user">
+            <form onSubmit={addItem}>
+                <label>First Name</label>
+                <input
+                    name='firstName'
+                    type="text"
+                    placeholder="Enter First Name"
+                    value={newUser.firstName}
+                    onChange={handleInputChange}
+                />
+                <label>Last Name</label>
+                <input
+                    name='lastName'
+                    type="text"
+                    placeholder="Enter Last Name"
+                    value={newUser.lastName}
+                    onChange={handleInputChange}
+                />
+                <label>Username</label>
+                <input
+                    name='userName'
+                    type="text"
+                    placeholder="Enter Username"
+                    value={newUser.userName}
+                    onChange={handleInputChange}
+                />
+                <button disabled={ inputIsNotValid() }>Add New User</button>
+            </form>
+        </div>
+    )
+}
+
+export default NewUserInput
